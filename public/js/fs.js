2efeed76378b55cbeedcccda9e9e9c1118a3c13b
Mio <lex.antonov@gmail.com>
2013-09-27 22:21:30 +0400

    changes admin panel

--- public/js/fs.js
+++ public/js/fs.js
@@ -1,111 +1,194 @@
-/*global Element */
-(function( window, document ) {
-  'use strict';
+// /*global Element */
+// (function( window, document ) {
+//   'use strict';
 
-  var keyboardAllowed = typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element, // IE6 throws without typeof check
+//   var keyboardAllowed = typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element, // IE6 throws without typeof check
 
-    fn = (function() {
-      var fnMap = [
-        [
-          'requestFullscreen',
-          'exitFullscreen',
-          'fullscreenchange',
-          'fullscreen',
-          'fullscreenElement',
-          'fullscreenerror'
-        ],
-        [
-          'webkitRequestFullScreen',
-          'webkitCancelFullScreen',
-          'webkitfullscreenchange',
-          'webkitIsFullScreen',
-          'webkitCurrentFullScreenElement',
-          'webkitfullscreenerror'
+//     fn = (function() {
+//       var fnMap = [
+//         [
+//           'requestFullscreen',
+//           'exitFullscreen',
+//           'fullscreenchange',
+//           'fullscreen',
+//           'fullscreenElement',
+//           'fullscreenerror'
+//         ],
+//         [
+//           'webkitRequestFullScreen',
+//           'webkitCancelFullScreen',
+//           'webkitfullscreenchange',
+//           'webkitIsFullScreen',
+//           'webkitCurrentFullScreenElement',
+//           'webkitfullscreenerror'
 
-        ],
-        [
-          'mozRequestFullScreen',
-          'mozCancelFullScreen',
-          'mozfullscreenchange',
-          'mozFullScreen',
-          'mozFullScreenElement',
-          'mozfullscreenerror'
-        ]
-      ],
-      i = 0,
-      l = fnMap.length,
-      ret = {},
-      val,
-      valLength;
+//         ],
+//         [
+//           'mozRequestFullScreen',
+//           'mozCancelFullScreen',
+//           'mozfullscreenchange',
+//           'mozFullScreen',
+//           'mozFullScreenElement',
+//           'mozfullscreenerror'
+//         ]
+//       ],
+//       i = 0,
+//       l = fnMap.length,
+//       ret = {},
+//       val,
+//       valLength;
 
-      for ( ; i < l; i++ ) {
-        val = fnMap[ i ];
-        if ( val && val[1] in document ) {
-          for ( i = 0, valLength = val.length; i < valLength; i++ ) {
-            ret[ fnMap[0][ i ] ] = val[ i ];
-          }
-          return ret;
-        }
-      }
-      return false;
-    })(),
+//       for ( ; i < l; i++ ) {
+//         val = fnMap[ i ];
+//         if ( val && val[1] in document ) {
+//           for ( i = 0, valLength = val.length; i < valLength; i++ ) {
+//             ret[ fnMap[0][ i ] ] = val[ i ];
+//           }
+//           return ret;
+//         }
+//       }
+//       return false;
+//     })(),
 
-    screenfull = {
-      isFullscreen: document[ fn.fullscreen ],
-      element: document[ fn.fullscreenElement ],
+//     screenfull = {
+//       isFullscreen: document[ fn.fullscreen ],
+//       element: document[ fn.fullscreenElement ],
 
-      request: function( elem ) {
-        var request = fn.requestFullscreen;
+//       request: function( elem ) {
+//         var request = fn.requestFullscreen;
 
-        elem = elem || document.documentElement;
+//         elem = elem || document.documentElement;
 
-        // Work around Safari 5.1 bug: reports support for
-        // keyboard in fullscreen even though it doesn't.
-        // Browser sniffing, since the alternative with
-        // setTimeout is even worse
-        if ( /5\.1[\.\d]* Safari/.test( navigator.userAgent ) ) {
-          elem[ request ]();
-        } else {
-          elem[ request ]( keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT );
-        }
-      },
+//         // Work around Safari 5.1 bug: reports support for
+//         // keyboard in fullscreen even though it doesn't.
+//         // Browser sniffing, since the alternative with
+//         // setTimeout is even worse
+//         if ( /5\.1[\.\d]* Safari/.test( navigator.userAgent ) ) {
+//           elem[ request ]();
+//         } else {
+//           elem[ request ]( keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT );
+//         }
+//       },
 
-      exit: function() {
-        document[ fn.exitFullscreen ]();
-      },
+//       exit: function() {
+//         document[ fn.exitFullscreen ]();
+//       },
 
-      toggle: function( elem ) {
-        if ( this.isFullscreen ) {
-          this.exit();
-        } else {
-          this.request( elem );
-        }
-      },
+//       toggle: function( elem ) {
+//         if ( this.isFullscreen ) {
+//           this.exit();
+//         } else {
+//           this.request( elem );
+//         }
+//       },
 
-      onchange: function() {},
-      onerror: function() {}
-    };
+//       onchange: function() {},
+//       onerror: function() {}
+//     };
 
-  if ( !fn ) {
-    window.screenfull = null;
-    return;
+//   if ( !fn ) {
+//     window.screenfull = null;
+//     return;
+//   }
+
+//   document.addEventListener( fn.fullscreenchange, function( e ) {
+//     screenfull.isFullscreen = document[ fn.fullscreen ];
+//     screenfull.element = document[ fn.fullscreenElement ];
+//     screenfull.onchange.call( screenfull, e );
+//   });
+
+//   document.addEventListener( fn.fullscreenerror, function( e ) {
+//     screenfull.onerror.call( screenfull, e );
+//   });
+
+//   window.screenfull = screenfull;
+
+// })( window, document );
+
+
+// $('#toggle').click(function() {
+//   screenfull.toggle( $('#container')[0] );
+// });
+
+
+// var fsButton = document.getElementById('toggle'),
+//     container = document.getElementById('container');
+
+
+// if (window.fullScreenApi.supportsFullScreen) {
+//     fsButton.style.display = 'block';
+
+//     fsButton.addEventListener('click', function() {
+//         if (fsButton.className == 'fs-off') {
+//             window.fullScreenApi.requestFullScreen(container);
+//         } else {
+//             window.fullScreenApi.cancelFullScreen(container);
+//         }
+//     }, true);
+
+//     container.addEventListener(fullScreenApi.fullScreenEventName, function() {
+//         if (fullScreenApi.isFullScreen()) {
+//             container.style.width = container.style.height = '100%';
+//             fsButton.className = 'fs-on';
+//         } else {
+//             fsButton.className = 'fs-off';
+//         }
+//     }, true);
+// } else {
+//     // no fullscreen support - do nothing
+// }
+
+
+document.cancelFullScreen = document.webkitExitFullscreen || document.mozCancelFullScreen || document.exitFullscreen;
+
+var elem = document.querySelector(document.webkitExitFullscreen ? "#fs" : "#container");
+
+
+function toggleFS(el) {
+  if (el.webkitEnterFullScreen) {
+    el.webkitEnterFullScreen();
+  } else {
+    if (el.mozRequestFullScreen) {
+      el.mozRequestFullScreen();
+    } else {
+      el.requestFullscreen();
+    }
   }
 
-  document.addEventListener( fn.fullscreenchange, function( e ) {
-    screenfull.isFullscreen = document[ fn.fullscreen ];
-    screenfull.element = document[ fn.fullscreenElement ];
-    screenfull.onchange.call( screenfull, e );
-  });
+  el.ondblclick = exitFullscreen;
+}
 
-  document.addEventListener( fn.fullscreenerror, function( e ) {
-    screenfull.onerror.call( screenfull, e );
-  });
+function onFullScreenEnter() {
+  console.log("Entered fullscreen!");
+  elem.onwebkitfullscreenchange = onFullScreenExit;
+  elem.onmozfullscreenchange = onFullScreenExit;
+};
 
-  window.screenfull = screenfull;
+// Called whenever the browser exits fullscreen.
+function onFullScreenExit() {
+  console.log("Exited fullscreen!");
+};
 
-})( window, document );
+// Note: FF nightly needs about:config full-screen-api.enabled set to true.
+function enterFullscreen() {
+  console.log("enterFullscreen()");
+  elem.onwebkitfullscreenchange = onFullScreenEnter;
+  elem.onmozfullscreenchange = onFullScreenEnter;
+  elem.onfullscreenchange = onFullScreenEnter;
+  if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  } else {
+    if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else {
+      elem.requestFullscreen();
+    }
+  }
+  document.getElementById('toggle').onclick = exitFullscreen;
+}
 
-
-$('#toggle').click(function() {
-  screenfull.toggle( $('#container')[0] );
-});
+function exitFullscreen() {
+  console.log("exitFullscreen()");
+  document.cancelFullScreen();
+  document.getElementById('toggle').onclick = enterFullscreen;
+}
