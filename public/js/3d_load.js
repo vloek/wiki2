36d0a586d9e9f448240bdff7a567987f1cdeee14
Lex <lex.antonov@gmail.com>
2014-03-03 13:02:12 +0400

    upgrade main menu angle

--- public/js/3d_load.js
+++ public/js/3d_load.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('.persp').css('margin-top', '50px');
   var marginMenuItem = $(document).width() * 0.1;
+  var marginTopMenuItem = $(document).height() / 7;
+  $('.perspectiva').css('height', marginTopMenuItem)
   $('.menu_item').css('margin-left', marginMenuItem);
 });
